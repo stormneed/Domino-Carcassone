587259140e8fff80175194bb6cb7bce9cc7f3f13
stormneed <matheodesousa935@gmail.com>
2022-12-23 23:28:59 +0100

    correction

--- src/main/java/Table.java
+++ src/main/java/Table.java
@@ -14,16 +14,16 @@ public class Table {
 
 
     public boolean estPosable (Tuile piece, int coordX, int coordY) {
-        if (coordX>0 && plateau[coordX-1][coordY]!=null &&!plateau[coordX-1][coordY].bords[2].equals(piece.bords[0])) {
+        if (coordX>0 && plateau[coordX-1][coordY]!=null &&!(plateau[coordX-1][coordY].bords[2]==piece.bords[0])) {
             return false;
         }
-        if (coordX<plateau.length-1 && plateau[coordX+1][coordY]!=null && !plateau[coordX+1][coordY].bords[0].equals(piece.bords[2])) {
+        if (coordX<plateau.length-1 && plateau[coordX+1][coordY]!=null && !(plateau[coordX+1][coordY].bords[0]==piece.bords[2])){
             return false;
         }
-        if (coordY>0 && plateau[coordX][coordY-1]!=null && !plateau[coordX][coordY-1].bords[1].equals(piece.bords[3])) {
+        if (coordY>0 && plateau[coordX][coordY-1]!=null && !(plateau[coordX][coordY-1].bords[1]==piece.bords[3])) {
             return false;
         }
-        if (coordY<plateau.length-1 && plateau[coordX][coordY-1]!=null &&  !plateau[coordX][coordY+1].bords[3].equals(piece.bords[1])) {
+        if (coordY<plateau.length-1 && plateau[coordX][coordY+1]!=null &&  !(plateau[coordX][coordY+1].bords[3]==piece.bords[1])) {
             return false;
         }
         return true;
