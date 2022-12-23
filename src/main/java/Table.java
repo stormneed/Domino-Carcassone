f01b581f034426a534af9ab17d4f3ee530bfa6c3
stormneed <matheodesousa935@gmail.com>
2022-12-24 00:18:24 +0100

    ajout de l'affichage dans la console

--- src/main/java/Table.java
+++ src/main/java/Table.java
@@ -1,5 +1,7 @@
 package main.java;
 
+import java.util.Objects;
+
 public class Table {
     Tuile[][] plateau;
 
@@ -14,19 +16,63 @@ public class Table {
 
 
     public boolean estPosable (Tuile piece, int coordX, int coordY) {
-        if (coordX>0 && plateau[coordX-1][coordY]!=null &&!(plateau[coordX-1][coordY].bords[2]==piece.bords[0])) {
+        if (coordX>0 && plateau[coordX-1][coordY]!=null &&!(Objects.equals(plateau[coordX - 1][coordY].bords[2], piece.bords[0]))) {
             return false;
         }
-        if (coordX<plateau.length-1 && plateau[coordX+1][coordY]!=null && !(plateau[coordX+1][coordY].bords[0]==piece.bords[2])){
+        if (coordX<plateau.length-1 && plateau[coordX+1][coordY]!=null && !(Objects.equals(plateau[coordX + 1][coordY].bords[0], piece.bords[2]))){
             return false;
         }
-        if (coordY>0 && plateau[coordX][coordY-1]!=null && !(plateau[coordX][coordY-1].bords[1]==piece.bords[3])) {
+        if (coordY>0 && plateau[coordX][coordY-1]!=null && !(Objects.equals(plateau[coordX][coordY - 1].bords[1], piece.bords[3]))) {
             return false;
         }
-        if (coordY<plateau.length-1 && plateau[coordX][coordY+1]!=null &&  !(plateau[coordX][coordY+1].bords[3]==piece.bords[1])) {
+        if (coordY<plateau.length-1 && plateau[coordX][coordY+1]!=null &&  !(Objects.equals(plateau[coordX][coordY + 1].bords[3], piece.bords[1]))) {
             return false;
         }
-        return true;
+        return plateau[coordX][coordY]==null;
+    }
+
+    public void pose(Tuile piece, int coordX, int coordY){
+        if(estPosable(piece,coordX,coordY)){
+            this.plateau[coordX][coordY]=piece;
+        }
+        else{
+            System.out.println("Placement impossible");
+        }
+    }
+
+    public void afficheCons(){
+        for (Tuile[] tuiles : plateau) {
+            for (Tuile tuile : tuiles) {
+                System.out.print((char) 27 + "[0m ");
+                if (tuile == null) {
+                    System.out.print("*** |");
+                } else {
+                    System.out.print(tuile.bords[0] + " |");
+                }
+            }
+            for (int j = 0; j < 3; j++) {
+                System.out.println();
+                for (Tuile tuile : tuiles) {
+                    if (tuile == null) {
+                        System.out.print("*   *|");
+                    } else {
+                        System.out.print(tuile.bords[3].charAt(j) + "   " + tuile.bords[1].charAt(j) + "|");
+                    }
+                }
+            }
+
+            System.out.println();
+            for (Tuile tuile : tuiles) {
+                System.out.print((char) 27 + "[4m ");
+                if (tuile == null) {
+                    System.out.print("*** |");
+                } else {
+                    System.out.print(tuile.bords[2] + " |");
+                }
+            }
+            System.out.println();
+
+        }
     }
 
 }
