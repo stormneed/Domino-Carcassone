18caeb99269299163a56ca5e96b60f505b89ebc4
Eric Debessel <sarow.sgz@gmail.com>
2022-12-27 16:57:22 +0100

    Amelioration sac

--- src/main/java/Sac.java
+++ src/main/java/Sac.java
@@ -15,6 +15,64 @@ public class Sac {
     public void ajouter (Tuile piece) {
         contenu.add(piece);
     }
+
+    public void remplirSacDefaut () {
+        Tuile tc = new Tuile("212","222","303","040");
+        Tuile t1 = new Tuile("104","032","122","212");
+        Tuile t2 = new Tuile("401","032","040","032");
+        Tuile t3 = new Tuile("212","312","312","230");
+        Tuile t4 = new Tuile("123","032","212","213");
+        Tuile t5 = new Tuile("104","212","321","230");
+        Tuile t6 = new Tuile("221","140","213","040");
+        Tuile t7 = new Tuile("040","032","104","041");
+        Tuile t8 = new Tuile("213","140","212","230");
+        Tuile t9 = new Tuile("212","211","213","041");
+        Tuile t10 = new Tuile("123","040","140","112");
+        Tuile t11 = new Tuile("312","230","211","222");
+        Tuile t12 = new Tuile("401","040","321","032");
+        Tuile t13 = new Tuile("312","211","032","222");
+        Tuile t14 = new Tuile("041","222","321","112");
+        Tuile t15 = new Tuile("112","312","321","040");
+        Tuile t16 = new Tuile("123","104","303","213");
+        Tuile t17 = new Tuile("303","123","140","401");
+        Tuile t18 = new Tuile("230","401","303","321");
+        Tuile t19 = new Tuile("123","040","312","104");
+        Tuile t20 = new Tuile("123","140","401","222");
+        Tuile t21 = new Tuile("303","211","104","041");
+        Tuile t22 = new Tuile("041","041","212","112");
+        Tuile t23 = new Tuile("303","211","321","140");
+        Tuile t24 = new Tuile("213","222","401","112");
+        contenu.add(tc);
+        contenu.add(t1);
+        contenu.add(t2);
+        contenu.add(t3);
+        contenu.add(t4);
+        contenu.add(t5);
+        contenu.add(t6);
+        contenu.add(t7);
+        contenu.add(t8);
+        contenu.add(t9);
+        contenu.add(t10);
+        contenu.add(t11);
+        contenu.add(t12);
+        contenu.add(t13);
+        contenu.add(t14);
+        contenu.add(t15);
+        contenu.add(t16);
+        contenu.add(t17);
+        contenu.add(t18);
+        contenu.add(t19);
+        contenu.add(t20);
+        contenu.add(t21);
+        contenu.add(t22);
+        contenu.add(t23);
+        contenu.add(t24);
+
+
+
+
+
+    }
     private void genereSacRec(int nbr){
         Tuile tmp=contenu.getLast();
         for (int i = 0; i < 4; i++) {
@@ -41,6 +99,7 @@ public class Sac {
     public Tuile piocher () {
         Tuile t = contenu.getFirst();
         contenu.removeFirst();
+        melanger();
         return t;
     }
 
--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,9 +2,9 @@ public class Test {
     
 
     public static void main (String[] args) {
-        Table t = new Table(5);
+        Table t = new Table(7);
         Sac s = new Sac(t);
-        s.genereSac(10);
+        s.remplirSacDefaut();
         PartieCons p = new PartieCons(2, t, s);
         p.jouerPartieCons();
 
