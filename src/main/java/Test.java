52914f90307287df99fce50a02cc69511db439bb
stormneed <matheodesousa935@gmail.com>
2023-01-06 16:27:38 +0100

    fignolage

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -5,18 +5,20 @@ public class Test {
     
 
     public static void main (String[] args) {
-        Table t = new Table(7);
-        Sac s = new Sac(t);
-        s.remplirSacDefautCarcasonne();
-        //PartieCons p = new PartieCons(3, 2, t, s);
-        //p.jouerPartieCons();
+
+        /** Pour jouer en console **/
+
+//        Table t = new Table(7);
+//        Sac s = new Sac(t);
+//        s.remplirSacDefautCarcasonne();
+//        PartieCons p = new PartieCons(3, 2, t, s);
+//        p.jouerPartieCons();
+
+        /** Pour jouer en Graphique **/
+
+
         Window p=new Window(2,1);
         p.setVisible(true);
-        //TuileCarc tuile=new TuileCarc("V","R","P","R",false, false,false);
-        //TuileGraph tg=new TuileGraph(tuile);
-        //if(p. content.getComponent(0) instanceof PartieGraph){
-        //    ((PartieGraph)p. content.getComponent(0)).poser(3,3,tg);
-        //}
 
 
 
